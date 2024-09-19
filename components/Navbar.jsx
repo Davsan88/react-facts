48ab6e1f87d57afd988fdbe7d35256d407640c60
DavSan <david.sanchez.martinez@outlook.com>
2024-09-19 13:28:00 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import '../App.css'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navBarDiv">
       <img src="/logo512.png" alt="React logo" style={{ width: '50px' }}/>
       <h1>ReactFacts</h1>
     </div>
