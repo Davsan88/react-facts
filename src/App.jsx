37af2bf3cd84b5fc2cd2eafc0c572b1c7cd4f0c3
DavSan <david.sanchez.martinez@outlook.com>
2024-09-19 12:07:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from '../components/Navbar'
+import Main from '../components/Main'
 import './App.css'
 
 function App() {
   return (
-    
-    <div style={{ margin: '20px'}}>
-        <img src="/logo512.png" alt="React logo" style={{ width: '50px' }}/>
-        <h1>Fun facts about React</h1>
-        <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
+    <>
+      <Navbar />
+      <Main />
+    </>
+
+    // <div style={{ margin: '20px'}}>
+    //     <img src="/logo512.png" alt="React logo" style={{ width: '50px' }}/>
+    //     <h1>Fun facts about React</h1>
+    //     <ul>
+    //     <li>Was first released in 2013</li>
+    //     <li>Was originally created by Jordan Walke</li>
+    //     <li>Has well over 100k stars on Github</li>
+    //     <li>Is maintained by Facebook</li>
+    //     <li>Powers thousands of enterprise apps, including mobile apps</li>
+    //     </ul>
+    // </div>
   )
 }
 
