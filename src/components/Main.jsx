a0a9efaf4a5ca9b53a6f8d522b2bfd6f2b810d9d
DavSan <david.sanchez.martinez@outlook.com>
2024-09-20 18:54:45 +0100

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,15 @@ import '../App.css'
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main goes here</h1>
+    <div className=''>
+      <h1 className=''>Fun facts about React</h1>
+      <ul className=''>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   )
 }
