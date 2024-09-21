e42cdfe6900dfdc81066681ebec8c16e4b0f1e3c
DavSan <david.sanchez.martinez@outlook.com>
2024-09-21 18:54:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,6 @@ function App() {
       <Navbar />
       <Main />
     </div>
-
-    // <div style={{ margin: '20px'}}>
-    //     <img src="/logo512.png" alt="React logo" style={{ width: '50px' }}/>
-    //     <h1>Fun facts about React</h1>
-    //     <ul>
-    //     <li>Was first released in 2013</li>
-    //     <li>Was originally created by Jordan Walke</li>
-    //     <li>Has well over 100k stars on Github</li>
-    //     <li>Is maintained by Facebook</li>
-    //     <li>Powers thousands of enterprise apps, including mobile apps</li>
-    //     </ul>
-    // </div>
-  )
 }
 
 export default App
