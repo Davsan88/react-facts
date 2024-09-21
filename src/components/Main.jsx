44152ab721163f3b08bb0c08d0f36973c7df3201
DavSan <david.sanchez.martinez@outlook.com>
2024-09-21 17:57:09 +0100

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,8 +8,8 @@ const Main = () => {
       <ul className="main__facts-list">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook</li>
+        <li>Has well over 200k stars on Github</li>
+        <li>Is maintained by Meta</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
     </main>
