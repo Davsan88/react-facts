41b247b56f05968d999af2a77439a5b55bc7a4a6
DavSan <david.sanchez.martinez@outlook.com>
2024-09-21 14:35:55 +0100

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,9 +3,9 @@ import '../App.css'
 
 const Main = () => {
   return (
-    <div className=''>
-      <h1 className=''>Fun facts about React</h1>
-      <ul className=''>
+    <div className='main'>
+      <h1 className='main__heading'>Fun facts about React</h1>
+      <ul className='main__facts-list'>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on Github</li>
